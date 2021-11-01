6729a99b72036268c8969bded43fdcf26f390a12
Apple <fenrirockz@gmail.com>
2021-11-01 22:59:06 +0300

    addede procject

---
+++ L15-Git-Alert/L15-Git-Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L15-Git-Alert
+//
+//  Created by Apple on 1.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
